3416fb27e82b12c2887981430f14e9ee0b86e8a6
Taylor Vann <brian.t.vann@gmail.com>
2025-07-03 11:52:06 -0700

    available_encodings

--- response/src/available_encodings.rs
+++ response/src/available_encodings.rs
@@ -15,22 +15,19 @@ impl AvailableEncodings {
             zstd: false,
         };
 
-        av_enc.update(potential_encodings);
-        av_enc
-    }
-
-    pub fn update(&mut self, potential_encodings: &Option<Vec<String>>) {
         if let Some(encodings) = potential_encodings {
             for encoding in encodings {
                 match encoding.as_str() {
-                    "gzip" => self.gzip = true,
-                    "deflate" => self.deflate = true,
-                    "br" => self.br = true,
-                    "zstd" => self.zstd = true,
+                    "gzip" => av_enc.gzip = true,
+                    "deflate" => av_enc.deflate = true,
+                    "br" => av_enc.br = true,
+                    "zstd" => av_enc.zstd = true,
                     _ => {}
                 }
             }
         }
+
+        av_enc
     }
 
     pub fn encoding_is_available(&self, encoding: &str) -> bool {
